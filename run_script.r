5bef35da42748d20e9fb702ab2a0a9f392a5484c
RAVINENDRA PRATAP <Ravi-Pratap@users.noreply.github.com>
2018-11-19 00:57:06 +1000

    Running all 4 Plotting Scripts at Once

---
+++ run_script.r
@@ -0,0 +1,12 @@
+
+# This Course: Exploratory Data Analysis: Plotting
+# FileName: run_script.R
+# Ravinendra Pratap
+# Submission date 18.11.2018
+{
+source("plot1.R")
+source("plot2.R")
+source("plot3.R")
+source("plot4.R")
+
+}
